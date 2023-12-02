7d93697e06e8af29a479f9525aeca2c46d85e3cb
Tushant <Tushant8826@gmail.com>
2023-12-02 17:50:56 +0530

    update-latest-version

---
+++ client/build/precache-manifest.9c837f1fb2dd4c17558735459c31bd05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "083accd318c1fb0719c7",
+    "url": "/static/css/main.24a6e8b1.chunk.css"
+  },
+  {
+    "revision": "083accd318c1fb0719c7",
+    "url": "/static/js/main.083accd3.chunk.js"
+  },
+  {
+    "revision": "090dbcf4a522cc8e7c4d",
+    "url": "/static/js/1.090dbcf4.chunk.js"
+  },
+  {
+    "revision": "47a35038665677ed7892",
+    "url": "/static/css/2.4e69c232.chunk.css"
+  },
+  {
+    "revision": "47a35038665677ed7892",
+    "url": "/static/js/2.47a35038.chunk.js"
+  },
+  {
+    "revision": "9eed903a2e0b97d43a56",
+    "url": "/static/js/runtime~main.9eed903a.js"
+  },
+  {
+    "revision": "24c39eef2cdcdedd938b6c846205bf74",
+    "url": "/index.html"
+  }
+];
